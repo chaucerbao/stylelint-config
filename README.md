d092e12b7cf319a9f655a882e0db08082e3f85fa
Alvin Chan <aschan@gmail.com>
2020-06-27 15:52:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Stylelint Recommended Preset
+
+## Installation
+```
+npm install --save-dev @chaucerbao/stylelint-config
+```
+
+## Usage
+Add this preset to the `extends` property of your **.stylelintrc**
+```
+{
+  "extends": ["@chaucerbao/stylelint-config"]
+}
+```
---
+++ src/index.js
@@ -0,0 +1,12 @@
+// Export the Stylelint configuration preset
+module.exports = {
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-prettier/recommended',
+  ],
+  plugins: ['stylelint-scss'],
+  rules: {
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+  },
+}
